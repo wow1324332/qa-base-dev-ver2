d82b8b3a763067c536c79c4c6885e7324f1c86ba
wow1324332 <wow1324332@gmail.com>
2026-05-27 14:23:52 +0900

    Update and rename firebase.js to firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyATKKSrUm6NKATdZdJeDxhQ5Dj2Q32ujh0",
   authDomain: "q-base-dev.firebaseapp.com",
@@ -12,7 +10,5 @@ const firebaseConfig = {
   appId: "1:756427289812:web:217c6ebb1bfbd1d931f741"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
