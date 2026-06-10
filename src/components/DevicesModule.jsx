0911c5d341ab8a8d02494b04c3cd500d8c8b3411
wow1324332 <wow1324332@gmail.com>
2026-06-11 08:14:50 +0900

    Update DevicesModule.jsx

--- src/components/DevicesModule.jsx
+++ src/components/DevicesModule.jsx
@@ -770,7 +770,7 @@ export const DevicesDashboard = ({ user, onNavigate, onLogout, onQuit }) => {
 
   return (
     <div className="w-screen h-screen bg-[#f8f9fa] flex flex-col overflow-hidden animate-simple-fade">
-      <header className="h-20 px-8 flex justify-between items-center bg-[url('/header-bg.jpg')] bg-cover bg-[length:100%_100%]">
+      <header className="h-20 px-8 flex justify-between items-center bg-[url('/header-bg.jpg')] bg-cover bg-[length:100%_100%] shrink-0 relative z-50 shadow-[0_15px_30px_-5px_rgba(0,0,0,0.5)]">
         <div className="flex items-center space-x-3"></div>
         <div className="flex items-center space-x-4">
           <div className="flex items-center space-x-2 bg-gray-50 px-3 py-1.5 rounded-full border border-gray-200 shadow-sm hover-breath cursor-default">
