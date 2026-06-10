d79097af4ff04ea85244d301bb703dbab429f540
wow1324332 <wow1324332@gmail.com>
2026-06-10 16:03:59 +0900

    Update ProjectsModule.jsx

--- src/components/ProjectsModule.jsx
+++ src/components/ProjectsModule.jsx
@@ -551,7 +551,7 @@ export const ProjectsDashboard = ({ user, onNavigate, onLogout, onQuit }) => {
         
         {/* 1. 블러 처리된 배경 레이어 (여기서 깔끔하게 닫혀야 함) */}
         <div 
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat blur-[42px] scale-[1.02] z-0 pointer-events-none"
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat blur-[1px] scale-[1.02] z-0 pointer-events-none"
           style={{ backgroundImage: "url('/project-bg.jpg')" }}
         ></div>
 
