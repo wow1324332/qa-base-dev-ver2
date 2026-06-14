57af78a50faf30315df2358b9eb31e80101b3e5e
wow1324332 <wow1324332@gmail.com>
2026-06-15 07:56:08 +0900

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -170,6 +170,7 @@ export const LoginScreen = ({ onLogin, onInstallApp }) => {
       
       <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-gray-200/40 rounded-full mix-blend-multiply filter blur-3xl opacity-50 animate-pulse"></div>
       <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-gray-300/40 rounded-full mix-blend-multiply filter blur-3xl opacity-50 animate-pulse" style={{ animationDelay: '2s'}}></div>
+      </div>
     </div>
   );
 };
